57ce2cd8add1cd4108fe5a940f829b5e7e7780b6
Moabe Batista <moabebatista@gmail.com>
2022-03-07 13:52:39 -0300

    Iniciando projeto

---
+++ src/controladores/alunos.js
@@ -0,0 +1,7 @@
+const listarAlunos = async (req, res) => {
+    res.json('Listando Alunos!');
+}
+
+module.exports = {
+    listarAlunos
+}
---
+++ src/rotas.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const alunos = require('./controladores/alunos');
+
+const rotas = express();
+
+rotas.get('/alunos', alunos.listarAlunos)
+
+module.exports = rotas;
