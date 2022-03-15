7770929298a4251aca0599240971dae5cb7554b5
Moabe Batista <moabebatista@gmail.com>
2022-03-14 22:19:57 -0300

    adicionando a dependencia 'cors' que permite a integraçao da api com o fronte e modificando porta no host que sera rodado a API(backend) para nao haver conflito com a que rodara a UI(frontend)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const rotas = require('./rotas');
 
-const PORT = 3000;
+
+const PORT = 3333;
 const HOST = "0.0.0.0"
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(rotas);
 
 app.listen(PORT, HOST);
